4b758e5af83c771ca9f6d5ac4ad948853e26107d
SIV\y.reyes <y.reyes@siv.gov.do>
2018-02-23 10:18:19 -0400

    Initial commit from netbeans!

---
+++ Machine Learning/src/com/yhomarth/src/YhoMain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yhomarth.src;
+
+/**
+ *
+ * @author y.reyes
+ */
+public class YhoMain {
+    
+    public static void main(String[] args) {
+        System.out.println("where everyting start!!");
+    }
+    
+}
