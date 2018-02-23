c47822ee7f01dccb761c465c41d6b2b580e02835
SIV\y.reyes <y.reyes@siv.gov.do>
2018-02-23 16:41:38 -0400

    Initial commit from netbeans!

--- Machine Learning/src/com/yhomarth/src/YhoMain.java
+++ Machine Learning/src/com/yhomarth/src/YhoMain.java
@@ -5,6 +5,13 @@
  */
 package com.yhomarth.src;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  *
  * @author y.reyes
@@ -13,6 +20,41 @@ public class YhoMain {
     
     public static void main(String[] args) {
         System.out.println("where everyting start!!");
-    }
+        
+        try{
+            URL url = new URL("https://en.wikipedia.org/wiki/Data_science");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            
+            System.out.println("Response code : " + 
+                    connection.getResponseCode());
+            
+            System.out.println("Content Type : " + connection.getContentType());
+            System.out.println("Content Length : " + connection.getContentLength());
+            
+            InputStreamReader isr = new InputStreamReader( 
+                    (InputStream) connection.getContent()  );
+            
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder buffer = new StringBuilder();
+            
+            String line;
+            
+            do{
+                line = br.readLine();
+                buffer.append(line + "\n");
+            } while (line != null);
+            
+            System.out.println(buffer.toString());          
+            
+            
+            
+        }//end of try 
+        catch( IOException ex){
+                System.out.println("Error : " + ex.getMessage());
+        }//end of MalformedURLException 
+        
+    }//end of main method
     
-}
+}//end of class
